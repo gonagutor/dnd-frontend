19fb674624f32db6bda8664a06d3f5ed8560357a
DamSergio <dam_sergioballesteros@riberadeltajo.es>
2023-09-05 23:07:43 +0200

    Empezar componente campaigns

---
+++ src/app/components/Campaigns.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+export default function Campaigns() {
+  const { t } = useTranslation('titles');
+  return (
+    <CampaignsContainer>
+      <CampaignsTitle>{t('campaignList')}</CampaignsTitle>
+      <CampaingList />
+    </CampaignsContainer>
+  );
+}
+
+const CampaignsContainer = styled.div`
+  flex-shrink: 0;
+  margin: 1rem;
+`;
+
+const CampaignsTitle = styled.h2`
+  color: var(--text-lighter, #d3bb7c);
+  text-shadow: 0px 0px 4px #301005;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  margin-bottom: 1rem;
+`;
+
+function CampaingList() {
+  const campaigns: JSX.Element[] = [];
+  return <div>{campaigns}</div>;
+}
